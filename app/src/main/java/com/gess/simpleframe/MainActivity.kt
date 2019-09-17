9404fd0c32decb63f375d6db9d741309611fd3c6
zhenglei <zhenglei@5.cn>
2019-09-17 18:22:07 +0800

    frame

--- app/src/main/java/com/gess/simpleframe/MainActivity.kt
+++ app/src/main/java/com/gess/simpleframe/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.gess.simpleframe
 
+import android.app.Activity
 import android.view.View
 import android.widget.Toast
 import com.gess.core.base.presenter.BaseActivityPersenter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivityPersenter<MainDelegate>(), View.OnClickListener {
     override fun onClick(p0: View?) {
-        Toast.makeText(this,"成功",Toast.LENGTH_LONG).show()
+       toast("succeed")
+        tv_kotlin.text = "import kotlinx.android.synthetic.main.activity_main.*"
     }
 
     override fun bindEvenListener() {
         super.bindEvenListener()
         viewDelegate?.setOnClickListener(this, R.id.tv_kotlin)
     }
+}
+
+fun Activity.toast(msg: String){
+    Toast.makeText(applicationContext, msg, Toast.LENGTH_LONG).show()
 }
--- app/src/main/java/com/gess/simpleframe/MainDelegate.kt
+++ app/src/main/java/com/gess/simpleframe/MainDelegate.kt
@@ -1,15 +1,17 @@
 package com.gess.simpleframe
 
+import android.app.Activity
 import com.gess.core.base.view.BaseDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainDelegate() : BaseDelegate(){
+class MainDelegate : BaseDelegate(){
 
     override fun getRootLayoutId(): Int = R.layout.activity_main
 
-    override fun getOptionsMenuId(): Int =0
+    override fun getOptionsMenuId(): Int = 0
 
     override fun initWidget() {
-
+        getActivity<Activity>().tv_kotlin.text = "sssasdassd"
     }
 
 }
